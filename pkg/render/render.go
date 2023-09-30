2475720319aecbbfe8b5a06ae721e7527831e490
thekay <kasraay98@gmail.com>
2023-09-30 16:35:26 -0400

    Added initial template caching mechanism

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,17 +1,70 @@
 package render
 
 import (
-	"fmt"
+	"bytes"
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
 func RenderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	// create a template cache
+	tc, err := createTemplateCache()
+	if err != nil {
+		log.Fatal(err)
+	}
+	// get requested template from cache
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal(err)
+	}
 
-	err := parsedTemplate.Execute(w, nil)
+	buf := new(bytes.Buffer)
+
+	err = t.Execute(buf, nil)
 
 	if err != nil {
-		fmt.Println("Errors are: ", err)
+		log.Println(err)
 	}
+	// render the template
+	_, err = buf.WriteTo(w)
+	if err != nil {
+		log.Println(err)
+	}
+}
+func createTemplateCache() (map[string]*template.Template, error) {
+
+	myCache := map[string]*template.Template{}
+	// get all the files named *.page.gohtml
+	pages, err := filepath.Glob("./templates/*.page.gohtml")
+	if err != nil {
+		return myCache, err
+	}
+	// range through the files and create a template cache for each glob
+
+	for _, page := range pages {
+		name := filepath.Base(page)
+		ts, err := template.New(name).ParseFiles(page)
+		if err != nil {
+			return myCache, err
+		}
+
+		matches, err := filepath.Glob("./templates/*.layout.gohtml")
+		if err != nil {
+			return myCache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.gohtml")
+			if err != nil {
+				return myCache, err
+			}
+		}
+
+		myCache[name] = ts
+
+	}
+
+	return myCache, nil
 }
