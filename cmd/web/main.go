0b33a139825ecfd3825c6ddb5bd0044931a77923
thekay <kasraay98@gmail.com>
2023-10-01 12:54:48 -0400

    Added Sessions functionality

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,17 +2,32 @@ package main
 
 import (
 	"fmt"
+	"github.com/alexedwards/scs/v2"
 	"github.com/thakay/Reservator/pkg/config"
 	"github.com/thakay/Reservator/pkg/handlers"
 	"github.com/thakay/Reservator/pkg/render"
 	"log"
 	"net/http"
+	"time"
 )
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+
+var session *scs.SessionManager
+
 func main() {
-	var app config.AppConfig
+
+	//change when in production
+	app.InProduction = false
+	// session info
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
@@ -29,7 +44,7 @@ func main() {
 	//http.HandleFunc("/", handlers.Repo.Home)
 	//http.HandleFunc("/about", handlers.Repo.About)
 
-	fmt.Printf("starting appliaction on port %s", portNumber)
+	fmt.Printf("starting appliaction on port %s \n", portNumber)
 	//_ = http.ListenAndServe(portNumber, nil)
 
 	srv := &http.Server{
--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -27,6 +27,9 @@ func NewHandlers(r *Repository) {
 }
 
 func (m *Repository) Home(w http.ResponseWriter, r *http.Request) {
+	remoteIP := r.RemoteAddr
+	m.App.Session.Put(r.Context(), "remote_ip", remoteIP)
+
 	render.RenderTemplate(w, "home.page.gohtml", &models.TemplateData{})
 }
 
@@ -35,5 +38,7 @@ func (m *Repository) About(w http.ResponseWriter, r *http.Request) {
 	// perform some logic
 	stringMap := make(map[string]string)
 	stringMap["test"] = "Hello from Test"
+	remoteIP := m.App.Session.GetString(r.Context(), "remote_ip")
+	stringMap["remote_ip"] = remoteIP
 	render.RenderTemplate(w, "about.page.gohtml", &models.TemplateData{StringMap: stringMap})
 }
